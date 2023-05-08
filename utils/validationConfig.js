56d77c60fe48e5fda05f7b7f655b7f3ab422f2f1
Vladislav K <aljkeee@gmail.com>
2023-05-08 20:58:02 +0300

    fix: исправить Joi-валидацию для регистрации

--- utils/validationConfig.js
+++ utils/validationConfig.js
@@ -32,8 +32,8 @@ const validateRegistration = celebrate({
   body: Joi.object().keys({
     email: Joi.string().min(7).required().email(),
     password: Joi.string().required(),
-    name: Joi.string().min(2).max(30).required(),
-    about: Joi.string().min(2).max(30).required(),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
     avatar: Joi.string().pattern(regexp),
   }),
 });
